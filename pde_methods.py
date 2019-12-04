def3fee2dea5b8fe7f394491d025a4862ab8b760
rowan raikes <rr17592@bristol.ac.uk>
2019-12-04 12:55:27 +0000

    Implemented forward euler solution to heat equation in matrix form.

--- pde_methods.py
+++ pde_methods.py
@@ -20,6 +20,45 @@ def u_exact(x,t):
     y = np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L)
     return y
 
+def solve_heat_eq_matrix(mx, mt, L, T, kappa):
+
+    x = np.linspace(0, L, mx+1)     # mesh points in space
+    t = np.linspace(0, T, mt+1)     # mesh points in time
+    deltax = x[1] - x[0]            # gridspacing in x
+    deltat = t[1] - t[0]            # gridspacing in t
+    lmbda = kappa*deltat/(deltax**2) 
+
+    u_j = np.zeros(x.size)
+
+    # Set initial condition
+    for i in range(0, mx+1):
+        u_j[i] = u_I(x[i], L)
+
+    # create matrix
+    A_fe = np.zeros((x.size, x.size))
+
+    for i in range(x.size):
+        A_fe[i,i] = (1 - 2*lmbda)
+
+        if i < x.size - 1:
+            A_fe[i+1, i] = lmbda
+            A_fe[i, i+1] = lmbda
+
+    for n in range(mt):
+
+        u_jp1 = np.dot(A_fe, np.transpose(u_j) )
+
+        # Boundary conditions (outer mesh points)
+        u_jp1[0] = 0; u_jp1[mx] = 0
+        
+        # Update u_j
+        u_j[:] = u_jp1[:]
+
+
+    return(u_j)
+
+
+
 def solve_heat_eq(mx, mt, L, T, kappa):
 
     x = np.linspace(0, L, mx+1)     # mesh points in space
@@ -27,6 +66,9 @@ def solve_heat_eq(mx, mt, L, T, kappa):
     deltax = x[1] - x[0]            # gridspacing in x
     deltat = t[1] - t[0]            # gridspacing in t
     lmbda = kappa*deltat/(deltax**2) 
+
+    pj = 0   # boundary conditions
+    qj = 0   #
 
     descritisation = forward_euler_descritisation
 
@@ -40,24 +82,43 @@ def solve_heat_eq(mx, mt, L, T, kappa):
 
     for n in range(1, mt+1):
 
-        # Descritation at each time step
+        # Calculate inner mesh points
         for i in range(1, mx):
+
+
 
             u_jp1[i] = descritisation(u_j, u_jp1, lmbda, i)
 
         #print(u_jp1)
 
-        # Boundary conditions
-        u_jp1[0] = 0; u_jp1[mx] = 0
+        # Boundary conditions (outer mesh points)
+        u_jp1[0] = pj; u_jp1[mx] = qj
         
         # Update u_j
         u_j[:] = u_jp1[:]
 
     return u_j
 
+def forward_euler_dirchlet(u_j, u_jp1, lmbda, i, mx, pj, qj):
+
+
+    # first boundary timestep
+    if i == 1:
+        u_jp1 = u_j[i] + lmbda*(2*u_j[i] + u_j[i+1] + pj)
+
+    if i == mx:
+        u_jp1 = u_j[i] + lmbda*(u_j[i-1] - 2*u_j[i] + qj)
+
+    # normal timestep 
+    else:
+        u_jp1 = u_j[i] + lmbda*(u_j[i-1] - 2*u_j[i] + u_j[i+1])
+
+    return u_jp1
+
+
 def forward_euler_descritisation(u_j, u_jp1, lmbda, i):
 
-    # Forward Euler timestep   
+    # normal timestep 
     return u_j[i] + lmbda*(u_j[i-1] - 2*u_j[i] + u_j[i+1])
 
 def backward_euler_descritisation(u_j, u_jp1, lmbda, i):
@@ -71,24 +132,28 @@ def crank_nicholson_descritisation(u_j, u_jp1, lmbda, i):
     return u_j[i] + (lmbda / 2)*(u_jp1[i+1] - 2*u_jp1[i] + u_jp1[i-1] + u_j[i+1] - 2*u_j[i] + u_j[i-1])
 
 
-# # set numerical parameters
-# mx = 10     # number of gridpoints in space
-# mt = 1000 # number of gridpoints in time
+# set numerical parameters
+mx = 10     # number of gridpoints in space
+mt = 10    # number of gridpoints in time
 
-# # set problem parameters/functions
-# kappa = 1.0   # diffusion constant
-# L=1.0         # length of spatial domain
-# T=0.1        # total time to solve for
+# set problem parameters/functions
+kappa = 1.0   # diffusion constant
+L=1.0         # length of spatial domain
+T=0.01     # total time to solve for
 
-# u_j = solve_heat_eq(mx, mt, L, T, kappa)
+u_j = solve_heat_eq(mx, mt, L, T, kappa)
 
-# # plot the final result and exact solution
-# x = np.linspace(0, L, mx+1)
+print(u_j)
 
-# plt.plot(x,u_j,'ro',label='num')
-# xx = np.linspace(0,L,250)
-# plt.plot(xx,u_exact(xx,T),'b-',label='exact')
-# plt.xlabel('x')
-# plt.ylabel('u(x,0.5)')
-# plt.legend(loc='upper right')
-# plt.show()
+# plot the final result and exact solution
+x = np.linspace(0, L, mx+1)
+
+plt.plot(x,u_j,'ro',label='num')
+xx = np.linspace(0,L,250)
+plt.plot(xx,u_exact(xx,T),'b-',label='exact')
+plt.xlabel('x')
+plt.ylabel('u(x,0.5)')
+plt.legend(loc='upper right')
+plt.show()
+
+solve_heat_eq_matrix(mx, mt, L, T, kappa)
